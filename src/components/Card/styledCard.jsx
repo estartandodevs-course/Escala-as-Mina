16e6bee0bbccc89eeac6cec6273895404fb378a4
Sarah <sarah.abreu2012@gmail.com>
2020-11-28 17:04:18 -0300

    removed margin card component done

--- src/components/Card/styledCard.jsx
+++ src/components/Card/styledCard.jsx
@@ -6,7 +6,6 @@ export const StyledCard = styled.div.attrs((props) => ({
   box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.25);
   border-radius: 15px;
   padding: 16px;
-  margin: 50px;
   width: ${(props) => (props.size === 'large' ? '900px' : '327px')};
   height: ${(props) => (props.size === 'large' ? '652px' : '231px')};
 `;
