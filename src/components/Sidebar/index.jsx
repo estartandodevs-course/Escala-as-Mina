8ddbb0ffad91684704f195f0bef155d0fb8dd006
rednerdoficial <anapaulacirilo.santos@gmail.com>
2020-11-28 17:00:38 -0300

    Atualizando branchs

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -13,23 +13,25 @@ const Wrapper = styled.aside`
   ${(props) => props.styling.variation}: 0;
 `;
 
+
 export const Sidebar = (props) => {
+  const { variation = "left" } = props;
   const themeContext = useContext(ThemeContext);
   const stylingObject = {
     left: {
       variation: "left",
       borderRadius: "0 30px 30px 0",
       backgroundColor: `${themeContext.primaryColor}`,
-      width: "18vw", //18% of 1440px ~ 264px,
+      width: "clamp(260px, 18vw, 19vw)", //18% of 1440px ~ 264px,
     },
     right: {
       variation: "right",
       borderRadius: "30px 0 0 30px",
       backgroundColor: `${themeContext.tertiaryColor}`,
-      width: "25vw", //25% of 1440px ~ 356px
+      width: "clamp(356px, 25vw, 26vw)", //25% of 1440px ~ 356px
     },
   };
-  const { variation = "left" } = props;
   const styling = stylingObject[variation];
+
   return <Wrapper styling={styling}>{props.children}</Wrapper>;
 };
