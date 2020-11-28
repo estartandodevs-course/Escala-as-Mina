f83bc878491f298b740d6eb8d9599b7efb489f83
rednerdoficial <anapaulacirilo.santos@gmail.com>
2020-11-28 18:41:39 -0300

    importando sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { theme } from "./styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Login, Rodadas, Jogadoras, Times, Ranking, Dashboard } from "./pages";
 import { Sidebar } from "./components/Sidebar";
+import { Layout } from "./components/Layout";
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-
+      <Layout></Layout>
     </ThemeProvider>
   );
 }
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,8 @@
 import { ThemeProvider } from "styled-components";
-import { Sidebar } from "./components/Sidebar";
-import { Button } from "./components/Button";
-import styled, { css } from "styled-components";
+import { Sidebar } from "../Sidebar";
 
 
+export const Layout = (props) => {
+    const layoutSidebar = props;
+    return <Sidebar></Sidebar>;
+}
