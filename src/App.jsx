a379974329f31b490a11fd7d1015dae103a3c8b5
rednerdoficial <anapaulacirilo.santos@gmail.com>
2020-11-28 18:17:47 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,13 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./styles";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Login, Rodadas, Jogadoras, Times, Ranking, Dashboard } from "./pages";
+import { Sidebar } from "./components/Sidebar";
+import { layoutSidebar } from "./components/Layout";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Switch>
-          <Route path="/login">
-            <Login />
-          </Route>
-
-          <Route path="/rodadas">
-            <Rodadas />
-          </Route>
-
-          <Route path="/jogadoras">
-            <Jogadoras />
-          </Route>
-
-          <Route exact path="/">
-            <Dashboard />
-          </Route>
-
-          <Route path="/times">
-            <Times />
-          </Route>
-
-          <Route path="/ranking">
-            <Ranking />
-          </Route>
-        </Switch>
-      </Router>
+      <layoutSidebar></layoutSidebar>
     </ThemeProvider>
   );
 }
---
+++ src/components/Layout/layout.jsx
@@ -0,0 +1,9 @@
+import { ThemeProvider } from "styled-components";
+import { Sidebar } from "./components/Sidebar";
+import { Button } from "./components/Button";
+import styled, { css } from "styled-components";
+
+export const layoutSidebar = (props) => {
+    Sidebar;
+}
+
