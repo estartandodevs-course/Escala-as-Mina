6bbf2d62653c738e0ddbe88c2db263760907368c
rednerdoficial <anapaulacirilo.santos@gmail.com>
2020-11-29 23:49:06 -0300

    ideia de objeto no svg

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,91 +1,56 @@
-import { ThemeProvider } from "styled-components";
 import { Sidebar } from "../Sidebar";
 import { useTheme } from "styled-components";
 import { Button } from "../Button";
-import styled from "styled-components";
 import imgLogo from "../../assets/images/Logo.png";
-import Dashboard from "../../assets/icons/Group-2.svg";
+import Dashboard from "../../assets/icons/Dashboard.svg";
 import AdicionarRodada from "../../assets/icons/AdicionarSumula.svg";
 import AvaliarTimesUsuarios from "../../assets/icons/AvaliarTimesUsuarios.svg";
 import TimesCadastrados from "../../assets/icons/AvaliarJogadoras.svg";
-import UnsetIcon from "../../assets/icons/Group.svg";
+import RankingIcon from "../../assets/icons/Group.svg";
 import ExitIcon from "../../assets/icons/Exit.svg";
-import { Card } from "../Card"
-import { NavLink as NavLinkRouter } from "react-router-dom";
-// Estou usando algumas tamanhos do figma e outros estou fazendo de olho pois não tem. Precisamos decidir o que fazer sobre a resposividade.
-// NavLink
-
-const Link = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 0px;
-    justify-content: space-evenly;
-    height: 239px;
-`
-//styled da div que contém os NavLink agrupados.
-const NavLink = styled(NavLinkRouter)`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    img {
-        margin-right: 15px;
-    }
-    &.actived {
-        background-color: #f00;
-    }
-`
-// Styled a div que contem o button e o icon.
-// verificar o margin-right acima.
-
-const ContainerLogo = styled.div`
-    img {
-        width: 169px;
-        height: 164px;
-        margin-top: 18px;
-        margin-bottom: 60px;
-    }
- `
-  // Esse Container recebe a logo do DFF.
-  // verificar as margins da logo e o tamanho. Foi usado o mesmo do figma para o tamanho e as magins estão no olho.
-
- const ContainerLayout = styled.div`
-    display: flex;
-    flex-direction: row;
-    height: 100%;
+import { Card } from "../Card";
+import {Link, NavLink, ContainerLayout, ContainerLogo, PageContents, ContainerNav, ExitContainer, Svg} from "./StyledLayout";
 
 
- `
- // esse styled é a div que vai englobar todo o Sidebar e o conteúdo da página da page (PageContents)
 
-const ExitContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin-top: auto;
-    margin-bottom: 32px;
-    img {
-        margin-right: 15px;
-    }
-`
-// esse styled é a div que contem o botão e o icon de "Sair"
-
-
-const ContainerNav = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-`
-
-// esse styled a div que contém todos os botões (NavLink e ExitContainer) que vão receber o router.
-
-const PageContents = styled.div`
-    display: flex;
-    height: 100%;
-    width: 100%;
-    background: #f00;
-`
-// essa parte precisa ficar em row com o sidebar e não em cima dele e 100% da tela a partir do sidebar
-
+const navItems = [
+    {
+        text: "Dashboard",
+        src: "dashboard",
+        to: "/",
+        alt: "Icon Dashboard",
+    },
+    {
+        text: "Adicionar Rodada",
+        src: AdicionarRodada, //AdicionarSumula.svg
+        to: "/rodadas",
+        alt: "Icon Dashboard",
+    },
+    {
+        text: "Times Cadastrados",
+        src: TimesCadastrados, //AvaliarJogadoras.svg
+        to: "/jogadoras",
+        alt: "Icon Dashboard",
+    },
+    {
+        text: "Lorem",
+        src: AvaliarTimesUsuarios,
+        to: "/times",
+        alt: "Icon Dashboard",
+    },
+    {
+        text: "Ranking",
+        src: RankingIcon,
+        to: "/ranking",
+        alt: "Ranking Dashboard",
+    },
+    {
+        text: "Sair",
+        src: ExitIcon,
+        to: "/login",
+        alt: "Exit Dashboard",
+    },
+ ]
 
 
 export const Layout = () => {
@@ -96,36 +61,23 @@ export const Layout = () => {
                     <img src={imgLogo} alt="Logo DFF"/>
                 </ContainerLogo>
                 <ContainerNav>
-                    <Link>
-                        <NavLink to="/Maria" activeClassName="actived">
-                            <img src={Dashboard} alt="Icon Dashboard"/>
-                            <Button type="oulined" variation="secondary" size="small"> Dashboard</Button>
-                        </NavLink>
-                        <NavLink to="/" activeClassName="actived">
-                            <img src={AdicionarRodada} alt="Icon Adicionar Rodada"/>
-                            <Button type="oulined" variation="secondary" size="small">Adicionar Rodada</Button>
-                        </NavLink>
-                        <NavLink to="/" activeClassName="actived">
-                            <img src={TimesCadastrados} alt="Icon Times Cadastrados"/>
-                            <Button type="oulined" variation="secondary" size="small">Times Cadastrados</Button>
-                        </NavLink>
-                        <NavLink to="/" activeClassName="actived">
-                            <img src={AvaliarTimesUsuarios} alt=""/>
-                            <Button type="oulined" variation="secondary" size="small">Lorem ipsum</Button>
-                        </NavLink>
-                        <NavLink to="/" activeClassName="actived">
-                            <img src={UnsetIcon} alt=""/>
-                            <Button type="oulined" variation="secondary" size="small">Lorem ipsum</Button>
-                        </NavLink>
-                    </Link>
-                    <ExitContainer>
-                            <img src={ExitIcon} alt=""/>
-                            <Button type="outlined" variation="alert" size="small">Lorem ipsum</Button>
-                    </ExitContainer>
+                    {navItems.map((item, index, array) => {
+                        return (
+                            <NavLink to={item.to} activeClassName="actived">
+                                <Svg src={item.src} />
+                                {/* <img src={item.src} alt={item.alt} /> */}
+                                <Button type="oulined" variation={index === array.lenght-1 ? "alert" : "secondary"} size="small">{item.text}</Button>
+                            </NavLink>
+                        ) 
+                        })
+                    }
+                    <Button variation="active" size="small">
+                        Aparece pfv
+                    </Button>
                 </ContainerNav>
             </Sidebar>
             <PageContents>
-                <Card></Card>
+
             </PageContents>
         </ContainerLayout>
     )
@@ -134,27 +86,5 @@ export const Layout = () => {
 
 
 //tenho que pensar como vou fazer o navLink augeee
-// preciso colocar o ExitContainer no fim do ContainerNav e arrumar um jeito para o Sidebar e o PageContents fique em row, sem ficar em cima do outro, e o PageContents pegar 100% da tela a partir do Sidebar
 
-// // const navItems = [
-//     {
-//         text: "Dashboard",
-//         src: dashboard
-//     },
-//     {
-//         text: "Adicionar Rodada",
-//         src: sumula
-//     },
-//     {
-//         text: "Times Cadastrados",
-//         src: jogadoras
-//     },
-//     {
-//         text: "Lorem ipsum",
-//         src: times
-//     },
-//     {
-//         text: "Lorem ipsum",
-//         src: group
-//     }
-// ]
+
--- src/styles/index.jsx
+++ src/styles/index.jsx
@@ -5,9 +5,11 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     margin: 0;
   }
-  #root {
+  :root {
     font-size: 10px;
-    height:100%
+  }
+  #root {
+    height: 100%;
   }
   body, html {
     height:100%;
