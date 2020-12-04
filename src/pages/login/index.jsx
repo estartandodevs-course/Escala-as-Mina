25cfd1c48dd392c3f6cf8d3c05de113425a62710
rednerdoficial <anapaulacirilo.santos@gmail.com>
2020-12-03 21:26:28 -0300

    tentando fazer o trem para validar e-mail de login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 import React from 'react';
-import ReactDOM from 'react-dom';
 import imgBackground from "../../assets/images/LoginBackground.png";
 import { createGlobalStyle } from 'styled-components'
 import LogoLogin from "../../assets/images/LogoLogin.png";
+import { Button } from "../../components/Button";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -28,6 +28,30 @@ const Logo = styled.div`
   justify-content: center;
 `
 
+function EmailValidation (field) {
+  user = props.value.substring(0, field.value.indexOf("@"));
+  dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
+
+  if ((user.length >=1) &&
+      (dominio.length >=3) &&
+      (user.search("@")==-1) &&
+      (dominio.search("@")==-1) &&
+      (user.search(" ")==-1) &&
+      (dominio.search(" ")==-1) &&
+      (dominio.search(".")!=-1) &&
+      (dominio.indexOf(".") >=1) &&
+      (dominio.lastIndexOf(".") < dominio.length - 1)) {
+        document.getElementById ().innerHTML="Tudo OK com seu e-mail!";
+        alert("email valido");
+      }
+  else {
+    document.getElementById ().innerHTML="<font color ='red'> Seu e-mail é inválido </font>";
+    alert("email invalido");
+  }
+}
+
+
+
 export const LoginLayout = (props) => {
     const { children } = props;
     return (
@@ -38,6 +62,7 @@ export const LoginLayout = (props) => {
                 <Logo>
                     <img src={LogoLogin} alt="DiarioFFemnino"/>
                 </Logo>
+
             </LoginBox>
         </React.Fragment>
 
