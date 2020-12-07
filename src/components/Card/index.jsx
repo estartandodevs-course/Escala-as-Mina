296780b3ded6bfcd0e796c3a519ed94c3a4b8650
Rodrigo Queiroz <ryaqueiroz@gmail.com>
2020-12-06 21:25:24 -0300

    Update index.jsx

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,12 @@
-
 import * as S from './styledCard';
+import { useTheme } from "styled-components";
 
 //props.size can be small, normal or large | Default = Small
 
 export const Card = (props) => {
   const { children, width, height, size = "small", ...restProps } = props;
-
+  const theme = useTheme();
+  
   const styling = {
     width: theme.dimensions.card[size].width,
     height: theme.dimensions.card[size].height,
