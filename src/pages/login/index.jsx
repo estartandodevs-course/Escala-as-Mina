646eb71fd7834c2a441e0b9f90b1d5f8d8436b42
rednerdoficial <anapaulacirilo.santos@gmail.com>
2020-12-07 22:10:09 -0300

    layout finalizado, input recriado

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -4,6 +4,7 @@ import imgBackground from "../../assets/images/LoginBackground.png";
 import { createGlobalStyle } from 'styled-components'
 import LogoLogin from "../../assets/images/LogoLogin.png";
 import { Button } from "../../components/Button";
+import "fontsource-roboto";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -27,10 +28,98 @@ const Logo = styled.div`
   display: flex;
   justify-content: center;`
 
-const LoginContainer = styled.form`
+const LoginContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  button {
+    margin-top: 60px;
+    font-family: "roboto";
+    font-style: normal;
+    font-weight: 900;
+    font-size: 18px;
+    line-height: 21.09px;
+  }
+
 
 `
+const ContainerUser = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-family: "roboto";
+  color: ${(props) => props.theme.pallete.secondary.lighter};
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  justify-content: space-between;
+  margin-top: 50px;
 
+  input{
+    display: flex;
+    align-items: center;
+    font-size: 1.6rem;
+    color: ${(props) => props.theme.pallete.gray.black};
+    font-weight: 500;
+    line-height: 19px;
+    padding-left: 16px;
+    height: 48px;
+    border: 2px solid ${(props) => props.theme.pallete.gray.firstGray};
+    border-radius: 10px;
+    width: 432px;
+    height: 47.4141px;
+    margin-top: 15px;
+
+    ::-webkit-inline-placeholder {
+      color: ${(props) => props.theme.pallete.gray.secondGray};
+    }
+
+    &:focus {
+      outline: none;
+      border: 2px solid ${(props) => props.theme.pallete.secondary.main};
+    }
+  }
+
+`
+const ContainerPassword = styled.form`
+  display: flex;
+  flex-direction: column;
+  font-family: "roboto";
+  color: ${(props) => props.theme.pallete.secondary.lighter};
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 28px;
+  justify-content: space-between;
+  margin-top: 30px;
+
+  input{
+    display: flex;
+    align-items: center;
+    font-size: 1.6rem;
+    color: ${(props) => props.theme.pallete.gray.black};
+    font-weight: 500;
+    line-height: 19px;
+    padding-left: 16px;
+    height: 48px;
+    border: 2px solid ${(props) => props.theme.pallete.gray.firstGray};
+    border-radius: 10px;
+    width: 432px;
+    height: 47.4141px;
+    margin-top: 15px;
+
+    ::-webkit-inline-placeholder {
+      color: ${(props) => props.theme.pallete.gray.secondGray};
+    }
+
+    &:focus {
+      outline: none;
+      border: 2px solid ${(props) => props.theme.pallete.secondary.main};
+    }
+  }
+
+`
 
 export const LoginLayout = (props) => {
     const { children } = props;
@@ -43,7 +132,29 @@ export const LoginLayout = (props) => {
                   <img src={LogoLogin} alt="DiarioFFemnino"/>
                 </Logo>
                 <LoginContainer>
-
+                  <ContainerUser>
+                    Email 
+                    <input 
+                    type="email" 
+                    placeholder="E-mail"
+                    >
+                    </input>
+                  </ContainerUser>
+                  <ContainerPassword>
+                    Senha
+                    <input 
+                    type="password" 
+                    placeholder="Senha"
+                    >
+                    </input>
+                  </ContainerPassword>
+                  <Button
+                    type="solid"
+                    variation="primary"
+                    size="normal"
+                  >
+                    Entrar
+                  </Button>
                 </LoginContainer>
             </LoginBox>
         </React.Fragment>
