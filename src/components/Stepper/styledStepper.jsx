aa85d35047c099ca0603cf3721546aea07eda3ac
teddiur <ryaqueiroz@gmail.com>
2020-12-09 12:59:41 -0300

    deleted commented code in styledStepper

--- src/components/Stepper/styledStepper.jsx
+++ src/components/Stepper/styledStepper.jsx
@@ -1,11 +1,9 @@
-import styled from "styled-components";
-import { getV } from "../../styles";
+import styled from 'styled-components';
+import { getV } from '../../styles';
 
 const Ball = styled.div`
-  /* width: ${getV("64px", "h")};
-  height: ${getV("64px", "h")}; */
-  width: ${getV("64px", "h")};
-  height: ${getV("64px", "h")};
+  width: ${getV('64px', 'h')};
+  height: ${getV('64px', 'h')};
   border-radius: 50%;
   opacity: ${(props) => props.opacity};
   background-image: ${(props) => props.theme.pallete.gradient.main};
@@ -15,10 +13,9 @@ const Ball = styled.div`
 const VerticalBar = styled.div`
   position: absolute;
   top: 100%;
-  /* right: ${getV("33px", "w")}; */
-  right: ${getV("28px", "h")};
+  right: ${getV('28px', 'h')};
   width: 2px;
-  height: ${getV("40px", "h")};
+  height: ${getV('40px', 'h')};
   background-image: ${(props) => props.theme.pallete.gradient.main};
   z-index: 1;
   opacity: 0.5;
