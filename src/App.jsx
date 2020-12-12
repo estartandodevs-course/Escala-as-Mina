26ba4207e263fdd5ec89305fdfafdc0204149f73
teddiur <ryaqueiroz@gmail.com>
2020-12-12 13:42:14 -0300

    firebase's service and config added

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,11 @@ import {
 import { LoginLayout, Dashboard, Rodadas, Times, Ranking } from "./pages";
 import { Layout } from "./components";
 import { useEffect, useState } from "react";
-import firebase from "firebase/app";
-import "firebase/auth";
-
-const credentials = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-};
-firebase.initializeApp(credentials);
-
-function onAuthStateChange(callback) {
-  return firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      callback({ loggedIn: true });
-    } else {
-      callback({ loggedIn: false });
-    }
-  });
-}
+import { onAuthStateChange } from "./service/auth.service";
+import "./config/auth.config";
 
 function App() {
   const [user, setUser] = useState({ loggedIn: false });
-
   useEffect(() => {
     const unsubscribe = onAuthStateChange(setUser);
 
