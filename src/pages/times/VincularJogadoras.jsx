2af11c242a2f033d9678998731fa11167249c737
Antoniel Magalhães <antoniel2210@gmail.com>
2021-01-23 20:59:47 -0300

    refactor: split VincularJogadoras in another component

---
+++ src/pages/times/VincularJogadoras.jsx
@@ -0,0 +1,52 @@
+import * as C from "../../components";
+import { useState } from "react";
+
+export const VincularJogadoras = () => {
+  const positionSection = "GOL";
+  const [players, setPlayers] = useState([
+    {
+      number: "01",
+      name: "Camile Perreira",
+      id: 1,
+      show: true,
+    },
+    {
+      number: "02",
+      name: "Eduarda Jasmim",
+      id: 2,
+      show: true,
+    },
+    {
+      number: "03",
+      name: "Cristina Santos",
+      id: 3,
+      show: false,
+    },
+  ]);
+
+  return (
+    <C.FlexContainer align="flex-start" justify="flex-start" direction="column">
+      {players.map((player, index) => {
+        if (player.show) {
+          return (
+            <C.ListItem
+              key={index}
+              variation="edit"
+              type="player"
+              set={setPlayers}
+              id={player.id}
+            >
+              <>{player.number}</>
+              <>{positionSection}</>
+              <>{player.name}</>
+            </C.ListItem>
+          );
+        } else {
+          return "";
+        }
+      })}
+
+      <C.CreatePlayer />
+    </C.FlexContainer>
+  );
+};
--- src/pages/times/index.jsx
+++ src/pages/times/index.jsx
@@ -1,63 +1,16 @@
-import * as C from "../../components";
 import { useState } from "react";
 import { CriandoUmClube } from "./CriandoUmClube";
+import { VincularJogadoras } from "./VincularJogadoras";
+import * as C from "../../components";
 
 export const Times = () => {
   // const [addTeam, setAddTeam] = useState(false);
   const [step, setStep] = useState(0);
-  const positionSection = "GOL";
-  const [players, setPlayers] = useState([
-    {
-      number: "01",
-      name: "Camile Perreira",
-      id: 1,
-      show: true,
-    },
-    {
-      number: "02",
-      name: "Eduarda Jasmim",
-      id: 2,
-      show: true,
-    },
-    {
-      number: "03",
-      name: "Cristina Santos",
-      id: 3,
-      show: false,
-    },
-  ]);
+  // const positionSection = "GOL";
 
   const addTeamPages = [
     <CriandoUmClube step={step} setStep={setStep} />,
-    <>
-      <C.FlexContainer
-        align="flex-start"
-        justify="flex-start"
-        direction="column"
-      >
-        {players.map((player, index) => {
-          if (player.show) {
-            return (
-              <C.ListItem
-                key={index}
-                variation="edit"
-                type="player"
-                set={setPlayers}
-                id={player.id}
-              >
-                <>{player.number}</>
-                <>{positionSection}</>
-                <>{player.name}</>
-              </C.ListItem>
-            );
-          } else {
-            return "";
-          }
-        })}
-
-        <C.CreatePlayer />
-      </C.FlexContainer>
-    </>,
+    <VincularJogadoras />,
   ];
   return (
     <>
